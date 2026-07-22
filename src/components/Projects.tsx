af00bd6b7414a30458d3ab539184bb75a7eda233
akram-hany-front-end <akramhanyahmed@gmail.com>
2026-07-23 01:47:39 +0300

    Bug  fixed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,49 +6,52 @@ import ShinyText from "./ShinyText";
 import { useTheme } from "next-themes";
 const projects = [
   {
-    id:1,
-src: "/pro1.png",
-desc: "A modern mobile e-commerce application that provides a seamless shopping experience with product browsing, secure authentication, cart management, and online checkout",
-url: "https://next-e-commerce-rouge.vercel.app/",
-
-},
+    id: 1,
+    src: "/pro1.png",
+    name: "E-commerce",
+    desc: "A modern mobile e-commerce application that provides a seamless shopping experience with product browsing, secure authentication, cart management, and online checkout",
+    url: "https://next-e-commerce-rouge.vercel.app/",
+  },
   {
-    id:2,
-src: "/pro2.png",
-desc: "A hotel management system that streamlines room reservations, guest management, check-ins, check-outs, and booking operations through an intuitive and efficient interface.",
-url: "https://next-hotel-system.vercel.app/dashboard/home",
+    id: 2,
+    src: "/pro2.png",
+        name: "Hotel-Management-System",
 
-},
+    desc: "A hotel management system that streamlines room reservations, guest management, check-ins, check-outs, and booking operations through an intuitive and efficient interface.",
+    url: "https://next-hotel-system.vercel.app/dashboard/home",
+  },
   {
-    id:3,
-src: "/pro3.png",
-desc: "A modern blog platform where users can create, edit, publish, and manage articles through a clean and responsive interface",
-url: "https://next-blog-omega-ruddy.vercel.app/",
+    id: 3,
+    src: "/pro3.png",
+        name: "Blog-Posts",
 
-},
+    desc: "A modern blog platform where users can create, edit, publish, and manage articles through a clean and responsive interface",
+    url: "https://next-blog-omega-ruddy.vercel.app/",
+  },
   {
-    id:4,
-src: "/pro4.png",
-desc: "A comprehensive school management system that simplifies student, teacher, class, and academic administration through an intuitive dashboard.",
-url: "/",
+    id: 4,
+    src: "/pro4.png",
+        name: "School-Management-System",
 
-},
+    desc: "A comprehensive school management system that simplifies student, teacher, class, and academic administration through an intuitive dashboard.",
+    url: "/",
+  },
   {
-    id:5,
-src: "/pro5.png",
-desc: "A modern and responsive portfolio website showcasing my projects, technical skills, and professional experience with smooth animations and an intuitive user interface",
-url: "/",
+    id: 5,
+    src: "/pro5.png",
+        name: "Personal-Portfolio",
 
-},
-
-]
+    desc: "A modern and responsive portfolio website showcasing my projects, technical skills, and professional experience with smooth animations and an intuitive user interface",
+    url: "/",
+  },
+];
 export default function Projects() {
   const { resolvedTheme } = useTheme();
 
   return (
     <section
       id="projects"
-      className="min-h-screen relative flex flex-col items-center gap-8 flex-wrap justify-center overflow-hidden py-20 bg-background text-foreground"
+      className="min-h-screen relative flex flex-col items-center gap-8  justify-center overflow-hidden py-20 bg-background text-foreground"
     >
       <ShinyText
         text="My Projects"
@@ -64,28 +67,30 @@ export default function Projects() {
         disabled={false}
       />
 
-      <div className="flex flex-row items-center gap-3 flex-wrap justify-center">
+      <div className="flex flex-row items-center gap-3 flex-wrap justify-center ">
         {projects.map((project) => (
           <SpotlightCard
             key={project.id}
-            className="custom-spotlight-card"
+            className="custom-spotlight-card  w-100 flex flex-col items-center"
             spotlightColor={
               resolvedTheme === "dark" ? "#FFFFFF40" : "#00000030"
             }
           >
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-5 items-center">
               <Image
                 src={project.src}
                 alt="Project Image"
-                width={200}
+                width={300}
                 height={200}
                 className="rounded-xl"
               />
               <span className="text-xl font-semibold text-foreground">
-                Project name
+                {project.name}
               </span>
-              <p className="text-xs font-normal text-muted-foreground">
-                Lorem ipsum dolor sit amet.
+              <p className="text-[14px] font-normal text-muted-foreground w-[350px] break-words line-clamp-2 overflow-scroll ">
+
+              {project.desc}
+
               </p>
               <Link
                 className="text-sm text-black hover:underline  self-end"
